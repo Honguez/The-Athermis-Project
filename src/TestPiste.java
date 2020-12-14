5b03e916f47dba3817dbf36ade6e6fec2fe6a87b
Algoth39 <62031167+Algoth39@users.noreply.github.com>
2020-12-14 16:39:58 +0100

    Update TestPiste.java

--- src/TestPiste.java
+++ src/TestPiste.java
@@ -3,11 +3,14 @@ public class TestPiste {
 
     public static void main(String[] args) {
         Joueur j1 = new Joueur();
+        Joueur j2 = new Joueur();
         j1.setPosPisteSecours(5);
+        j2.setPosPisteSecours(4);
         System.out.println(j1.getPosPisteSecours());
 
         //j1.compensation();
         j1.choixRecompense();
+        j2.choixRecompense();
     }
 
 }
