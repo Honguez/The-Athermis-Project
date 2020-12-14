ee75a406c407251abdca30bc48c74e1e004b4b6c
Algoth39 <62031167+Algoth39@users.noreply.github.com>
2020-12-14 10:52:30 +0100

    Création class Joueur / PisteSecours

---
+++ src/Joueur.java
@@ -0,0 +1,8 @@
+public class Joueur {
+
+    int PosPisteSecours;
+
+    public Joueur(){
+        
+    }
+}
---
+++ src/PisteSecours.java
@@ -0,0 +1,18 @@
+
+
+
+public class PisteSecours {
+
+
+    public void compensation(){
+
+    }
+
+    public void choixRecompense(){
+
+
+    }
+
+
+
+}
